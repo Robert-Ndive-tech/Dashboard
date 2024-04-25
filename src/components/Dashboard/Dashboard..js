60a66c3c7f9963cda204cff153aa8eaecdff26e7
robert <robertndive29@gmail.com>
2024-04-25 22:55:19 +0100

    changes

--- src/components/Dashboard/Dashboard..js
+++ src/components/Dashboard/Dashboard..js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../index.css";
 import me from "../../assets/image1.png";
 import Sidediv from "../Sidediv";
+import vehicles from "../../assets/vehicles.png";
 import "../../styles/Homediv.css";
 const Dashboard = () => {
   return (
@@ -26,6 +27,18 @@ const Dashboard = () => {
       <div style={{ flex: 5 }} className="maindash">
         <div className="Tophead">
           <h2>VVMS Dashboard</h2>
+        </div>
+        <div>
+          <div className="center1">
+            <img src={vehicles} alt="imagea" width={300} height={100} />
+            <div>
+              <p id="me">
+                <span id="me1">10%</span> Decrease of the Total Employee
+              </p>
+              <p className="num">90</p>
+              <button className="button light-button">1.23</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
