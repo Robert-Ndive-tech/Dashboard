708f19c3d0944092ac2c499a346034f3a46278ce
robert <robertndive29@gmail.com>
2024-04-25 23:57:42 +0100

    changes

--- src/components/Dashboard/Dashboard..js
+++ src/components/Dashboard/Dashboard..js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../index.css";
 import me from "../../assets/image1.png";
+import vistor from "../../assets/Visitors.png";
 import Sidediv from "../Sidediv";
 import vehicles from "../../assets/vehicles.png";
+import Fvech from "../../assets/Fvehicles.png";
 import "../../styles/Homediv.css";
 const Dashboard = () => {
   return (
@@ -28,17 +30,56 @@ const Dashboard = () => {
         <div className="Tophead">
           <h2>VVMS Dashboard</h2>
         </div>
-        <div>
-          <div className="center1">
-            <img src={vehicles} alt="imagea" width={300} height={100} />
-            <div>
-              <p id="me">
-                <span id="me1">10%</span> Decrease of the Total Employee
-              </p>
-              <p className="num">90</p>
-              <button className="button light-button">1.23</button>
-            </div>
-          </div>
+        <div style={{ display: "inline-block" }}>
+          <table>
+            <head></head>
+            <tbody>
+              <td>
+                <div className="Box1">
+                  <img src={vistor} alt="imagea1" width={300} height={100} />
+                  <div>
+                    <div style={{ display: "block" }}>
+                      <button className="button light-button">1.23</button>
+                      <p className="num">90</p>
+                    </div>
+
+                    <p id="me">
+                      <span id="me2">10%</span> Increase of the Total Employee
+                    </p>
+                  </div>
+                </div>
+              </td>
+
+              <td>
+                <div className="center1">
+                  <img src={vehicles} alt="imagea" width={300} height={100} />
+                  <div>
+                    {" "}
+                    <button className="button light-button">1.23</button>
+                    <p className="num">90</p>
+                    <p id="me">
+                      <span id="me1">10%</span> Decrease of the Total Employee
+                    </p>{" "}
+                  </div>
+                </div>
+              </td>
+              {/*next box */}
+
+              <td>
+                <div className="Box4">
+                  <img src={Fvech} alt="imagea" width={300} height={100} />
+                  <div>
+                    <button className="button light-button1">1.23</button>
+                    <p className="num">90</p>
+
+                    <p id="me">
+                      <span id="me2">10%</span> Increase of the Total Employee
+                    </p>
+                  </div>
+                </div>
+              </td>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
