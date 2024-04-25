dd3416df4db10170612cce239562ef82eb5e0e40
robert <robertndive29@gmail.com>
2024-04-25 22:37:17 +0100

    changes

--- src/components/Dashboard/Dashboard..js
+++ src/components/Dashboard/Dashboard..js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../../index.css";
 import me from "../../assets/image1.png";
+import Sidediv from "../Sidediv";
+import "../../styles/Homediv.css";
 const Dashboard = () => {
   return (
-    <div className="dash">
+    <div className="dash" style={{ display: "flex" }}>
       <div className="Top">
         <img
           src={me}
@@ -14,11 +16,18 @@ const Dashboard = () => {
           style={{ float: "left" }}
         />
         <div className="search-input">
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Search..." name="my" />
         </div>
       </div>
 
-      <div></div>
+      <div style={{ flex: 1 }}>
+        <Sidediv />
+      </div>
+      <div style={{ flex: 5 }} className="maindash">
+        <div className="Tophead">
+          <h2>VVMS Dashboard</h2>
+        </div>
+      </div>
     </div>
   );
 };
